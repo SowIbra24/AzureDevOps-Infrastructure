b1f94292fd8e286ecac56d19424beb3fd5b4666a
p2019597 <ibrahima.sow@etu.univ-lyon1.fr>
2026-04-22 11:33:06 +0200

    Ajout des premières variables tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "nom_projet" {
+    description = "Le nom pour les ressources du projet"
+    type = string
+    default = "ibra-devops"
+}
+
+variable "region" {
+    description = "La localisation géographique des ressources"
+    type = string
+    default = "France Central"
+}
+
+variable "env" {
+  description = "Le type d'environnement (dev, test, prod)"
+  type        = string
+  default     = "dev"
+}
