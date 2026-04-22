d513f5d4477210f26b89b307fa0975e802ae250c
p2019597 <ibrahima.sow@etu.univ-lyon1.fr>
2026-04-22 14:11:45 +0200

    Appel du module network

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "rg_projet" {
   name     = "rg-${var.nom_projet}-${var.env}"
   location = var.region
 }
+
+module "reseau_principal" {
+    source = "./modules/network"
+
+    nom_du_vnet =  "vnet-${var.nom_projet}"
+    emplacement = azurerm_resource_group.rg_projet.location 
+    nom_du_rg = azurerm_resource_group.rg_projet.name 
+    adresse_vnet = ["10.0.0.0/16"] 
+}
