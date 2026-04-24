301a9a4982ebcf4c450f240ef04143e33cc0685c
p2019597 <ibrahima.sow@etu.univ-lyon1.fr>
2026-04-24 09:45:25 +0200

    Ajout des règles du firewall pour me permettre de ping et de me connecter en ssh sur la vm qui sera cree

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -11,3 +11,38 @@ resource "azurerm_subnet" "subnet_interne" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "nsg_projet" {
+  name                = "nsg-ibra-devops"
+  location            = var.emplacement
+  resource_group_name = var.nom_du_rg
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowPing"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "snet_assoc" {
+  subnet_id                 = azurerm_subnet.subnet_interne.id
+  network_security_group_id = azurerm_network_security_group.nsg_projet.id
+}
